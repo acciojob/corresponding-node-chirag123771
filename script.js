dc83be1d3baa76f98250132d26f32232cee46e2c
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 01:43:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-function correspondingNode(root1, root2, node1) {
-  
+function correspondingNode(tree1, tree2, node) {
+  if (!tree1 || !tree2 || !node) {
+        return null;
+    }
+
+    const pathToNode = getPathToNode(tree1, node);
+
+    let correspondingNode = tree2;
+    for (const index of pathToNode) {
+        correspondingNode = correspondingNode.children[index];
+    }
+
+    return correspondingNode;
+}
+
+function getPathToNode(root, targetNode) {
+    const path = [];
+
+    function findNode(currentNode) {
+        if (!currentNode) {
+            return false;
+        }
+
+        if (currentNode === targetNode) {
+            return true;
+        }
+
+        for (let i = 0; i < currentNode.children.length; i++) {
+            if (findNode(currentNode.children[i])) {
+                path.unshift(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    findNode(root);
+    return path;
 }
 
 module.exports=correspondingNode;
